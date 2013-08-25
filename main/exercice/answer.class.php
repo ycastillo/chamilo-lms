fb9deaea9dfa44130341c61e5bd9d7138b40622f
Yoselyn Castillo <ycastillo06@gmail.com>
2013-08-25 00:01:56 -0400

    fixes for fib and zone image questions -refs #6533

--- main/exercice/answer.class.php
+++ main/exercice/answer.class.php
@@ -446,7 +446,7 @@ class Answer {
 	function updateAnswers($answer,$comment, $correct, $weighting, $position, $destination, $hotspot_coordinates, $hotspot_type)
     {
 		$TBL_REPONSES = Database :: get_course_table(TABLE_QUIZ_ANSWER);
-
+        $id = $this->getQuestionType() == 3 ? intval($_REQUEST['myid']) : Database::escape_string($position);
 		$questionId=$this->questionId;
 		$sql = "UPDATE $TBL_REPONSES SET
                 answer = '".Database::escape_string($answer)."',
@@ -454,8 +454,10 @@ class Answer {
                 correct = '".Database::escape_string($correct)."',
 				ponderation = '".Database::escape_string($weighting)."',
 				position = '".Database::escape_string($position)."',
+                hotspot_coordinates = '".Database::escape_string($hotspot_coordinates)."',
+                hotspot_type = '".Database::escape_string($hotspot_type)."',  
 				destination = '".Database::escape_string($destination)."'
-				WHERE c_id = {$this->course_id} AND id = '".Database::escape_string($position)."'
+				WHERE c_id = {$this->course_id} AND id = $id
 				AND question_id = '".Database::escape_string($questionId)."'";
 		Database::query($sql);
 	}
